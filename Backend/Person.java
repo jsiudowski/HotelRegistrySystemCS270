be03460ab4486f6ef99526f5c85e2a921fb7ca7a
Jon Siudowski <jsiudowski25@jcu.edu>
2023-03-16 13:49:52 -0400

    Added constructors and methods to Person.java

--- Backend/Person.java
+++ Backend/Person.java
@@ -1,5 +1,56 @@
 package Backend;
 
-public class Person {
-    
+public abstract class Person {
+
+    public String name;
+    public int age;
+    protected String address;
+    public boolean employee;
+
+    public Person() {
+        name = "";
+        age = 0;
+        address = "";
+        employee = false;
+    }
+
+    public Person(String name, int age, String address, boolean employee) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.employee = employee;
+        
+    }
+
+    public void setName(Person person,String name) {
+        person.name = name;
+    }
+
+    public String getName(Person person) {
+        return person.name;
+    }
+
+    public void setAddress(Person person, String address) {
+        person.address = address;
+    }
+
+    protected String getAddress(Person person) {
+        return person.address;
+    }
+
+    public void setAge(Person person, int age) {
+        person.age = age;
+    }
+
+    public int getAge(Person person) {
+        return person.age;
+    }
+
+    public void setEmployee(Person person, boolean employeeStatus) {
+        person.employee = employeeStatus;
+    }
+
+    public boolean getEmployee(Person person) {
+        return person.employee;
+    }
 }
