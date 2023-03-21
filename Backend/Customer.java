963369af52fe32153c402327352702bfa7be2afc
jsiudowski <113116921+jsiudowski@users.noreply.github.com>
2023-03-21 12:43:13 -0400

    Created Employee.Java, Finished Customer.Java
    -Added constructors to Customer.Java
    -Added getters and setters to Customer.java
    -Created Employee.java

--- Backend/Customer.java
+++ Backend/Customer.java
@@ -1,5 +1,29 @@
 package Backend;
 
-public class Customer {
-    
+public class Customer extends Person{
+    public int customerID;
+    public String customerName;
+    public int customerAge;
+
+    public Customer() {
+        super();
+        customerID = 0;
+    }
+
+    public Customer(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public Customer(int customerID, String name, int age, String address, boolean isEmployee) {
+        super(name, age, address, isEmployee);
+        this.customerID = customerID;
+    }
+
+    public int getCustomerID(Customer customer) {
+        return customer.customerID;
+    }
+
+    public void setCustomerID(Customer customer, int newCustomerID) {
+        customer.customerID = newCustomerID;
+    }
 }
