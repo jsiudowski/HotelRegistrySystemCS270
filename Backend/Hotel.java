6398f63dc5bf3f65784f8b708f09ef1aafcb8cac
Vincent <v.wawak1@gmail.com>
2023-03-21 13:36:41 -0400

    Update hotel.java
    Built the framework for hotel.java

--- Backend/Hotel.java
+++ Backend/Hotel.java
@@ -1,5 +1,79 @@
 package Backend;
 
 public class Hotel {
-    
+
+    //Variables
+
+    public int roomAmount;
+    public String location;
+    public String amenities;
+    private int numEmployees;
+
+    //Constructors
+
+    public Hotel() {
+        this.roomAmount = 0;
+        this.location = "";
+        this.amenities = "";
+        this.numEmployees = 0;
+    }
+
+    public Hotel(int roomNum, String loc, String am, int numEm) {
+        if (roomNum >= 0) {
+            this.roomAmount = roomNum;
+        } else {
+            this.roomAmount = 0;
+        }
+
+        this.location = loc;
+        this.amenities = am;
+
+        if (numEm >= 0) {
+        this.numEmployees = numEm;
+        } else {
+            this.numEmployees = 0;
+        }
+    }
+
+    //Setters and Getters
+
+    public void setRoomAmount(int amt) {
+        if (amt >= 0) {
+            this.roomAmount = amt;
+        }
+    }
+
+    public int getRoomAmount() {
+        return this.roomAmount;
+    }
+
+    public void setLocation(String loc) {
+        this.location = loc;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    //Should amenities be stored inside of a String array?
+
+    public void setAmenities(String amenities) {
+        this.amenities = amenities;
+    }
+
+    public String getAmenities() {
+        return this.amenities;
+    }
+
+    public void setNumEmployees(int num) {
+        if (num >= 0) {
+            this.numEmployees = num;
+        }
+    }
+
+    public int getNumEmployees() {
+        return this.numEmployees;
+    }
+
+
 }
