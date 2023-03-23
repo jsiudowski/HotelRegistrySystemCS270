6eeb1cfd3d033c415a5895562724307ef7dd2208
Vincent <v.wawak1@gmail.com>
2023-03-23 13:34:14 -0400

    Update Floor.java
    Added content for Floor.java. Need to implement method in constructor that adds floor obj to list in hotel obj

--- Backend/Floor.java
+++ Backend/Floor.java
@@ -2,4 +2,33 @@ package Backend;
 
 public class Floor {
     
+    //Variables
+    public int floorNum;
+    public Hotel hotelId;
+
+    //Constructor
+    public Floor(Hotel hotel, int num) {
+        this.hotelId = hotel;
+        this.floorNum = num;
+    }
+
+    //Methods
+    public int getFloorNum(Hotel hotel) {
+        return this.floorNum;
+    }
+
+    public void setFloorNum(Hotel hotel, int num) {
+        this.floorNum = num;
+    }
+
+    public Hotel getFloorHotel() {
+        return this.hotelId;
+    }
+
+    public void setFloorHotel(Hotel hotel) {
+        this.hotelId = hotel;
+    }
+
+    //Its worth considering that a hotel can have mutliple floors
+
 }
