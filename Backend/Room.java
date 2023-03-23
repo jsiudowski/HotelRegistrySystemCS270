bbd78ecdc47fd70d4763ef680be91a4066a12ad5
nevelephant <aabeyesundere24@jcu.edu>
2023-03-23 13:20:42 -0400

    Update Room.java
    first stage of room class still have tp decide how to implement room into floor and hotel class

--- Backend/Room.java
+++ Backend/Room.java
@@ -1,5 +1,24 @@
 package Backend;
 
 public class Room {
+    public int roomNumber;
+    public String roomType;
+
+    public Room (int roomNumber, String roomType) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+    }
+
+
+    public int getRoomNum (Room room) {
+        return room.roomNumber;
+    }
+
+    public String getRoomType (Room room) {
+        return room.roomType;
+
+    }
+
+
     
 }
