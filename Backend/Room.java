9562f9fdb855fdd3d1e2ec57123958e4284a7415
nevelephant <aabeyesundere24@jcu.edu>
2023-03-23 13:41:49 -0400

    Update Room.java
    labeled stuff

--- Backend/Room.java
+++ Backend/Room.java
@@ -1,11 +1,13 @@
 package Backend;
 
 public class Room {
+    //variables
     public int roomNumber;
     public String roomType;
     public Hotel hotelId;
     public Floor floorNum;
-
+    
+    //construcor 
     public Room (Hotel hotel, Floor floor, int roomNumber, String roomType) {
         this.hotelId = hotel;
         this.floorNum = floor;
@@ -13,7 +15,7 @@ public class Room {
         this.roomType = roomType;
     }
 
-
+    //methods
     public int getRoomNum (Room room) {
         return room.roomNumber;
     }
