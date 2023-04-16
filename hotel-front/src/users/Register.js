1f83d07f5c55c7ff216722b39aa8e62c008917ab
Vincent <v.wawak1@gmail.com>
2023-04-16 16:26:06 -0400

    Updated Register.js
    Typing info into Register.js will now save that information into a js object

--- hotel-front/src/users/Register.js
+++ hotel-front/src/users/Register.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 
 export default function Register() {
 
+
+    //Object for users
     const [user, setUser]=useState({
         name:"",
         age:"",
@@ -10,6 +12,14 @@ export default function Register() {
         password:"",
         status:""
     })
+    
+    //Sets input values to user information at registration, check value attribute in inputs
+    const{name,age,address,username,password,status}=user;
+
+    //Function
+    const onInputChange=(e)=>{
+        setUser({...user,[e.target.name]:e.target.value})
+    }
 
   return (
     <div className="container">
@@ -20,54 +30,57 @@ export default function Register() {
                     <label htmlFor="Name" className="form-label">
                         Name
                     </label>
-                    <input type={"text"} className="form-control" name="name" placeholder="Enter Name"></input>
+                    <input type={"text"} className="form-control" name="name" placeholder="Enter Name" value={name} onChange={(e)=>onInputChange(e)}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Age" className="form-label">
                         Age
                     </label>
-                    <input type={"number"} className="form-control" name="age" placeholder="Enter Age"></input>
+                    <input type={"number"} className="form-control" name="age" placeholder="Enter Age" value={age} onChange={(e)=>onInputChange(e)}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Address" className="form-label">
                         Address
                     </label>
-                    <input type={"text"} className="form-control" name="address" placeholder="Enter Address"></input>
+                    <input type={"text"} className="form-control" name="address" placeholder="Enter Address" value={address} onChange={(e)=>onInputChange(e)}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Username" className="form-label" >
                         Username
                     </label>
-                    <input type={"text"} className="form-control" name="Username" placeholder="Enter Username"></input>
+                    <input type={"text"} className="form-control" name="username" placeholder="Enter Username" value={username} onChange={(e)=>onInputChange(e)}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Password" className="form-label">
                         Password
                     </label>
-                    <input type={"password"} className="form-control" name="Password" placeholder="Enter Password"></input>
+                    <input type={"password"} className="form-control" name="password" placeholder="Enter Password" value={password} onChange={(e)=>onInputChange(e)}></input>
                 </div>
                 <label className="form-label" htmlFor="RadioChecker">
                         Status
                 </label>
                 <div className="border rounded p-4 mt-2" id="RadioChecker">
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" name="StatusRadio" id="RadioOwner"/>
-                        <label className="form-check-label" for="RadioOwner">
-                            Owner
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" name="StatusRadio" id="RadioEmployee"/>
-                        <label className="form-check-label" for="RadioEmployee">
-                            Employee
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="radio" name="StatusRadio" id="RadioCustomer" />
-                        <label className="form-check-label" for="RadioCustomer">
-                            Customer
-                        </label>
-                    </div>
+                    <form onChange={(e)=>onInputChange(e)} value={status}>
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="status" id="RadioOwner" value={"Owner"} />
+                            <label className="form-check-label" for="RadioOwner">
+                                Owner
+                            </label>
+                        </div>
+                        <div className="form-check">
+                            
+                            <input className="form-check-input" type="radio" name="status" id="RadioEmployee" value={"Employee"} />
+                            <label className="form-check-label" for="RadioEmployee">
+                                Employee
+                            </label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="status" id="RadioCustomer" value={"Customer"} />
+                            <label className="form-check-label" for="RadioCustomer">
+                                Customer
+                            </label>
+                        </div>
+                    </form>
                 </div>
             </div>
             <div className="btn">
