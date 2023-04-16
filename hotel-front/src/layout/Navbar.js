64634c987ce9a29fdb9f800361aa998a42fe7766
Vincent <v.wawak1@gmail.com>
2023-04-16 12:52:45 -0400

    feat: Added Nav Bar to Homepage

--- hotel-front/src/layout/Navbar.js
+++ hotel-front/src/layout/Navbar.js
@@ -4,8 +4,22 @@ export default function Navbar() {
   return (
     <div>
 
-        
+        <nav className="navbar navbar-expand-lg navbar-light navbar-dark bg-info">
+            <div className="container-fluid">
+                <a href="#" className="navbar-brand" href="#">Navbar</a>
+                
+                <button className="navbar-toggler" 
+                    type="button" 
+                    data-toggle="collapse" 
+                    data-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
 
+                    
+                </button>
+            </div>
+        </nav>
     </div>
   )
 }
