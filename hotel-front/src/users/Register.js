91c5a6a87c0ea2ab316849e73fa7378bbbf83d30
Vincent <v.wawak1@gmail.com>
2023-04-16 15:55:17 -0400

    Added object for new users
    Added object for new users, and placeholders

--- hotel-front/src/users/Register.js
+++ hotel-front/src/users/Register.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Register() {
+
+    const [user, setUser]=useState({
+        name:"",
+        age:"",
+        address:"",
+        username:"",
+        password:"",
+        status:""
+    })
+
   return (
     <div className="container">
         <div className="row">
@@ -10,31 +20,31 @@ export default function Register() {
                     <label htmlFor="Name" className="form-label">
                         Name
                     </label>
-                    <input type={"text"} className="form-control" name="name"></input>
+                    <input type={"text"} className="form-control" name="name" placeholder="Enter Name"></input>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="Name" className="form-label">
+                    <label htmlFor="Age" className="form-label">
                         Age
                     </label>
-                    <input type={"number"} className="form-control" name="name"></input>
+                    <input type={"number"} className="form-control" name="age" placeholder="Enter Age"></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Address" className="form-label">
                         Address
                     </label>
-                    <input type={"text"} className="form-control" name="address"></input>
+                    <input type={"text"} className="form-control" name="address" placeholder="Enter Address"></input>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="Username" className="form-label">
+                    <label htmlFor="Username" className="form-label" >
                         Username
                     </label>
-                    <input type={"text"} className="form-control" name="Username"></input>
+                    <input type={"text"} className="form-control" name="Username" placeholder="Enter Username"></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Password" className="form-label">
                         Password
                     </label>
-                    <input type={"password"} className="form-control" name="Password"></input>
+                    <input type={"password"} className="form-control" name="Password" placeholder="Enter Password"></input>
                 </div>
                 <label className="form-label" htmlFor="RadioChecker">
                         Status
@@ -61,7 +71,8 @@ export default function Register() {
                 </div>
             </div>
             <div className="btn">
-                <input type={"submit"} className="btn btn-outline-dark"></input>
+                <button type={"submit"} className="btn btn-outline-dark mx-2">Submit</button>
+                <button type={"reset"} className="btn btn-outline-danger mx-2">Reset</button>
             </div>
         </div>
     </div>
