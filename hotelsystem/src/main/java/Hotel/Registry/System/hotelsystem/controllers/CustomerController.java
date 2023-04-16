a18ff16332e07aee2a2212424f436b32c13a6f48
Vincent <v.wawak1@gmail.com>
2023-04-16 11:47:27 -0400

    Added GETMapping to Customer
    Customer info can now be received via GET Request

--- hotelsystem/src/main/java/Hotel/Registry/System/hotelsystem/controllers/CustomerController.java
+++ hotelsystem/src/main/java/Hotel/Registry/System/hotelsystem/controllers/CustomerController.java
@@ -3,9 +3,12 @@ package Hotel.Registry.System.hotelsystem.controllers;
 import Hotel.Registry.System.hotelsystem.models.Customer;
 import Hotel.Registry.System.hotelsystem.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //Indicates that this class is a controller class
 @RestController
@@ -19,4 +22,10 @@ public class CustomerController {
     Customer newCustomer(@RequestBody Customer newCustomer) {
         return customerRepository.save(newCustomer);
     }
+
+    @GetMapping("/customers")
+    List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
 }
--- hotelsystem/src/main/java/Hotel/Registry/System/hotelsystem/models/Person.java
+++ hotelsystem/src/main/java/Hotel/Registry/System/hotelsystem/models/Person.java
@@ -8,6 +8,8 @@ public abstract class Person {
     public String name;
     public int age;
     public String address;
+
+
     public Person() {
         name = "";
         age = 0;
