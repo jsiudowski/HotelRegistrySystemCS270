34cdbba5579fcf990c2c82d8e762fde31fb0704b
Vincent <v.wawak1@gmail.com>
2023-04-16 09:14:57 -0400

    Created new Repository Files
    For an idea of what JPA Repositories are, check out https://www.geeksforgeeks.org/spring-boot-jparepository-with-example/#

---
+++ hotelsystem/src/main/java/Hotel/Registry/System/hotelsystem/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package Hotel.Registry.System.hotelsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository {
+
+}
---
+++ hotelsystem/src/main/java/Hotel/Registry/System/hotelsystem/repository/FloorRepository.java
@@ -0,0 +1,6 @@
+package Hotel.Registry.System.hotelsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FloorRepository extends JpaRepository {
+}
