581e88fbbddb1cd549f7998b351804bacf4caa45
Gavin Weiser <gavw2003@gmail.com>
2023-04-18 13:44:05 -0400

    Just added a Create/managehotel js file
    probably delete later on idk

---
+++ hotel-front/src/pages/Create&ManageHotel.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import React from 'react'
+
+export default function HotelForm() {
+  return (
+    <div>CreateManageHotel</div>
+  )
+}
