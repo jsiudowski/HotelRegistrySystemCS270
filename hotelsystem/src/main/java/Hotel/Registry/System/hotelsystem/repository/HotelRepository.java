d5ce155b5b7ec8a6879ec146f543c2d31bb2fd09
jsiudowski <113116921+jsiudowski@users.noreply.github.com>
2023-04-18 12:40:24 -0400

    Added HotelRepository.java

---
+++ hotelsystem/src/main/java/Hotel/Registry/System/hotelsystem/repository/HotelRepository.java
@@ -0,0 +1,7 @@
+package Hotel.Registry.System.hotelsystem.repository;
+
+import Hotel.Registry.System.hotelsystem.models.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface HotelRepository extends JpaRepository<Hotel, Floor>{
+
+}
