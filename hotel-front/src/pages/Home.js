aa82c841175d2c3481e9b441803653231edf2b5e
Abedelhadi Tawil <123099569+Atawil25@users.noreply.github.com>
2023-04-23 23:10:26 -0400

    Update Homepage
    I had some other stuff but I'm only putting this up for now because it did not come out the way I wanted.  We should probably discuss what exactly needs to go onto the homepage.

--- hotel-front/src/pages/Home.js
+++ hotel-front/src/pages/Home.js
@@ -1,7 +1,40 @@
 import React from 'react'
 
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className = 'container'>
+        <div className='py=4'>
+          <header>
+            <h1>Infinite Rooms</h1>
+          </header>
+
+          <br/>
+
+          <main>
+              <section id="find-room">
+                <h2>Find A Room</h2>
+                
+                  <div className ='location'>
+                    <div className= "col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                    <h2 className="text-center m-4">Where to?</h2>
+                    <div className="mb-4">
+                        <label htmlFor="location" className="form-location">
+                        </label>
+                        <input type={"text"} className="form-control" name="location" placeholder="Location"></input>
+                    </div>
+                    </div>
+                  </div>
+
+              </section>
+          <br/>
+              <section id="register-hotel">
+                <h2>Register A Hotel</h2>
+
+              </section>
+          </main>
+
+        </div>
+    </div>
   )
 }
