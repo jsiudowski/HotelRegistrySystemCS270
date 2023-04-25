26f34e15d79b9519f8d3f53d0321aba7f3813060
Gavin Weiser <gavw2003@gmail.com>
2023-04-25 19:55:48 -0400

    third times the charm
    yyyy

--- hotel-front/src/pages/Home.js
+++ hotel-front/src/pages/Home.js
@@ -10,8 +10,10 @@ export default function HomePage() {
           <img src ="https://media.cntraveler.com/photos/5be1b38dcfd2bb17f97a7a96/16:9/w_3999,h_2249,c_limit/Monastero-Santa-Rosa-Hotel-&-Spa__2018_MSRdusktviewproperty"></img>
         </div>
         <div class ="d-lg flex flex-column">
-          <div class = "home-page-info-black-border"></div>
-        
+          <div class = "home-page-info-black-border">
+            Choose a wide variety of rooms and services across multiple hotels
+          </div>
+        </div>
       </div>  
     </div>
   )
