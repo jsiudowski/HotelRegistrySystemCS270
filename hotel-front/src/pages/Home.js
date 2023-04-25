e408154a1b9e51546f817696a1efd789bd1bdeb3
Gavin Weiser <gavw2003@gmail.com>
2023-04-25 16:06:42 -0400

    Added minor stuff to flex on the Home.js and Index.css
    Have borders and tilte ready for home page
    
    [will work on images and other borders next]

--- hotel-front/src/pages/Home.js
+++ hotel-front/src/pages/Home.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>HomePage
+      <div class = "containter">
+        <h1></h1>
+        <div class ="d-lg-flex"> 
+          <div class = "title-black-border"> The Double Gulp Cup for the fucking slurpee
+          </div>
+        </div>
+      </div>  
+    </div>
   )
 }
