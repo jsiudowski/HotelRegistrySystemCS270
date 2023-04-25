004402d7ddb169ccfe4958f026c9c2e86049467a
Gavin Weiser <gavw2003@gmail.com>
2023-04-25 17:01:59 -0400

    Update to Home.JS Index.css
    >>Added an image and title font to home page
    
    >>Added the column border in index.css
    
    <<fix required for column border to fix text error

--- hotel-front/src/pages/Home.js
+++ hotel-front/src/pages/Home.js
@@ -5,10 +5,13 @@ export default function HomePage() {
     <div>HomePage
       <div class = "containter">
         <h1></h1>
-        <div class ="d-lg-flex"> 
-          <div class = "title-black-border"> The Double Gulp Cup for the fucking slurpee
-          </div>
+        <div class ="d-lg-flex flex row"> 
+          <div class = "home-page-title-black-border"> Welcome To Infinite Loop Services</div>
+          <img src ="https://media.cntraveler.com/photos/5be1b38dcfd2bb17f97a7a96/16:9/w_3999,h_2249,c_limit/Monastero-Santa-Rosa-Hotel-&-Spa__2018_MSRdusktviewproperty"></img>
         </div>
+        <div class ="d-lg flex flex-column">
+          <div class = "home-page-info-black-border"></div>
+        
       </div>  
     </div>
   )
