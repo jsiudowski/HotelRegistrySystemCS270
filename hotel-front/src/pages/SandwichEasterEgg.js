0bb82a55a9f90de9b0c1a89622ea7eb4493592a0
Gavin Weiser <gavw2003@gmail.com>
2023-04-27 12:38:23 -0400

    Color change to home.js

--- hotel-front/src/pages/SandwichEasterEgg.js
+++ hotel-front/src/pages/SandwichEasterEgg.js
@@ -6,11 +6,12 @@ export default function SandwichEasterEgg() {
 
    
     return (
-        <div> EasterEgg
+        <div>
             <title>HELLO</title>
             <h1>Go On</h1>
             <div class = "containter">
-                <img src="https://media.discordapp.net/attachments/980425522934849610/1082846748818538557/1660705096_Spinning_Sandwich.gif"></img>
+                <img src="https://media.discordapp.net/attachments/980425522934849610/1082846748818538557/1660705096_Spinning_Sandwich.gif"
+                width={600} height={500}></img>
             </div>
             <h2>Eat it</h2>
 
